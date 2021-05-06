cbab986bae1a3d896070ee1b7ccb14c20f0bc0b7
Weng-john <59682713+Weng-john@users.noreply.github.com>
2021-05-07 00:08:53 +0800

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,5 @@ function Confirm(){
     var person= document.getElementById("identity").value;
     person= person.split(" ");
     recorder= person[1];
-    
-    
+    document.getElementById("video").style.display= "block";
 }
