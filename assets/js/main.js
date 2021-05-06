00c706e17aaa614d348828fe55062288c76c140a
Weng-john <59682713+Weng-john@users.noreply.github.com>
2021-05-06 22:41:59 +0800

    test

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var recorder= "";
+function Confirm(){
+    var person= document.getElementById("identity").value;
+    person= person.split(" ");
+    recorder= person[1];
+    
+    
+}
---
+++ assets/js/scan.js
@@ -0,0 +1,21 @@
+var logindata= "";
+let scanner = new Instascan.Scanner({
+    continuous: true, // 連續掃描
+    video: document.getElementById('preview'), // 預覽
+    facingMode: {
+        exact: "environment"
+    }
+});
+scanner.addListener('scan', function (content) {
+    console.log(content);
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        scanner.start(cameras[0]); // [0] 前鏡頭 [1] 後鏡頭 
+    }
+    else {
+        console.error('沒有找到相機');
+    }
+    }).catch(function (e) {
+    console.error(e);
+});
