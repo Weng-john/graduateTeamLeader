341fed39906bd9631fbdada265e2856c7e3e83cf
Weng-john <59682713+Weng-john@users.noreply.github.com>
2021-05-07 13:57:09 +0800

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,36 @@
 var recorder= "";
+var index= 0;
+var Url=["https://script.google.com/macros/s/AKfycbypEbZDBf85xVhRbPcNRGFgGhjAtW1WpCjoq4fwr975Mqmwx7tQTXqbJitKFSCewwxR/exec", "https://script.google.com/macros/s/AKfycbwFUMz2HF51PjcmmJlbThrI4R0mMzOxfp3J4g2prltaPc1PptaKyliZDLr8HbQgK7KrsQ/exec"];
+
 function Confirm(){
     var person= document.getElementById("identity").value;
     person= person.split(" ");
     recorder= person[1];
     document.getElementById("video").style.display= "block";
+}
+
+function send(data){
+    $.ajax({
+            type:'get',
+            cache: false,
+            timeout: 6000,
+            url: Url[index%2],
+            data:  {
+                'data' : data
+            },
+            datatype:'json',
+            success: function(respond){
+                if(respond=="success"){
+                    alert("簽到成功");
+                }
+                else{
+                    alert("報到者資料填寫錯誤");
+                }
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){
+                    alert("抱歉，伺服器連線逾時\n將自動重傳資料，請稍後");
+                    index++;
+                    send(data);
+            }
+        });
 }
--- assets/js/scan.js
+++ assets/js/scan.js
@@ -1,4 +1,5 @@
 var logindata= "";
+
 let scanner = new Instascan.Scanner({
     continuous: true, // 連續掃描
     video: document.getElementById('preview'), // 預覽
@@ -6,20 +7,32 @@ let scanner = new Instascan.Scanner({
         exact: "environment"
     }
 });
+
 scanner.addListener('scan', function (content) {
     if(content!=""){
         content= content.split(",");
         content[1]= unescape(content[1].replace(/\\u/g, '%u'));
-        alert(content[1]);
+        content[2]= unescape(content[2].replace(/\\u/g, '%u'));
+        var correct= confirm("請確認名字是否無誤：\n" + content[1]);
+        
+        if(correct){
+            var Content= content.toString();
+            Content += "," + recorder;
+            send(Content);
+        }
+        else{
+            alert("請報到者重填資料");
+        }
     }
 });
+
 Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
         scanner.start(cameras[1]); // [0] 前鏡頭 [1] 後鏡頭 
     }
     else {
-        console.error('沒有找到相機');
+        alert('沒有找到相機');
     }
     }).catch(function (e) {
-    console.error(e);
+        alert(e);
 });
