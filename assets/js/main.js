b7e7023cc1b8f565c6c1274d93527c7e5f6038df
Weng-john <59682713+Weng-john@users.noreply.github.com>
2021-05-08 18:00:47 +0800

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,41 @@
 var recorder= "";
-var index= 0;
+var isConfirm= false;
 var press= false;
 var Url=["https://script.google.com/macros/s/AKfycbyMexMAWsW0V5zjbc2OnM4PYbyYS74yDchfiWgT/exec", "https://script.google.com/macros/s/AKfycbwDSgpyUX-X3Pyu7_9Hg92HGjXjQXz8bKvmmxv8HQ/exec"];
 
+var u = navigator.userAgent;
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+alert(isiOS);
+
 function Confirm(){
-    var person= document.getElementById("identity").value;
-    person= person.split(" ");
-    recorder= person[1];
-    document.getElementById("video").style.display= "block";
-    press= true;
+    if(!(isConfirm)){
+        var person= document.getElementById("identity").value;
+        person= person.split(" ");
+        recorder= person[1];
+    
+        if(!(isiOS)){
+            alert("創造section中");
+            press= true;
+            $("section").after("<section id="+ "video" +" class="+"video"+"></section>");
+            var insertDiv = document.getElementById("video");
+            insertDiv.innerHTML = "<video id=" + "preview " + "class=" + "image" + "></video>";
+            notiOS();
+        }
+    }
 }
 
-function send(data){
+function checkTime(Content){
+    var index= 0;
+    var Time= new Date();
+    var min= Time.getMinutes();
+    min %= 10;
+    
+    if(min>5)
+        index=1;
+    send(Content, index);
+}
+
+function send(data, index){
     $.ajax({
             type:'get',
             cache: false,
@@ -33,7 +57,7 @@ function send(data){
             error: function(XMLHttpRequest, textStatus, errorThrown){
                     alert("抱歉，伺服器連線逾時\n將自動重傳資料，請稍後");
                     index++;
-                    send(data);
+                    send(data, index);
             }
         });
 }
--- assets/js/scan.js
+++ assets/js/scan.js
@@ -1,43 +1,44 @@
 var logindata= "";
-/*
-    facingMode: {
-        exact: "environment"
-    }*/
 
-let scanner = new Instascan.Scanner({
-    continuous: true, // 連續掃描
-    video: document.getElementById('preview') // 預覽
-});
+function notiOS(){
+    let scanner = new Instascan.Scanner({
+        continuous: true, // 連續掃描
+        video: document.getElementById('preview') // 預覽
+    });
 
-scanner.addListener('scan', function (content) {
-    if(content!="" && press){
-        press= false;
-        content= content.split(",");
-        content[1]= unescape(content[1].replace(/\\u/g, '%u'));
-        content[2]= unescape(content[2].replace(/\\u/g, '%u'));
-        var correct= confirm("請確認資料是否正確：\n" + content[0] + "　" + content[1]);
+    scanner.addListener('scan', function (content) {
+        if(content!="" && press){
+            press= false;
+            content= content.split(",");
+            content[1]= unescape(content[1].replace(/\\u/g, '%u'));
+            content[2]= unescape(content[2].replace(/\\u/g, '%u'));
+            var correct= confirm("請確認資料是否正確：\n" + content[0] + "　" + content[1]);
         
-        if(correct){
-            var Content= content.toString();
-            Content += "," + recorder;
-            send(Content);
+            if(correct){
+                var Content= content.toString();
+                Content += "," + recorder;
+                checkTime(Content);
+            }
+            else{
+                alert("請報到者重填資料");
+                press= true;
+            }
         }
-        else{
-            alert("請報到者重填資料");
-            press= true;
-        }
-    }
-});
+    });
 
-Instascan.Camera.getCameras().then(function (cameras) {
-    if (cameras.length > 0) {
-        alert("有找到鏡頭，共有" + cameras.length + "個");
-        scanner.start(cameras[1]); // [0] 前鏡頭 [1] 後鏡頭 
-        scanner.mirror= false;
-    }
-    else {
-        alert('沒有找到相機');
-    }
-    }).catch(function (e) {
-        alert(e);
-});
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[1]); // [0] 前鏡頭 [1] 後鏡頭 
+            scanner.mirror= false;
+        }
+        else {
+            alert('沒有找到相機');
+        }
+        }).catch(function (e) {
+            alert(e);
+    });
+}
+
+function iOS(){
+    
+}
